7588e80fd8f12583bf68a47f5f03dc1abbc0dbf8
Mark Wilson <markwilson107@hotmail.com>
2020-11-14 21:54:20 +0800

    Menu and View all employees added.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 require("dotenv").config();
 
+// Classes
+const Department = require("./lib/Department");
+const Employee = require("./lib/Employee");
+const Roles = require("./lib/Roles");
+
 // MYSQL SETUP
 var connection = mysql.createConnection({
     host: "localhost",
@@ -23,5 +28,154 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    //querySongs();
+    init();
 });
+
+// Start Application
+function init() {
+    console.log("------------------------------------------------------------------------------");
+    console.log("---------------------   WELCOME TO EMPLOYEE TRACKER   ------------------------");
+    console.log("------------------------------------------------------------------------------");
+    initMenu();
+};
+
+// Main menu
+function initMenu() {
+    inquirer
+        .prompt({
+            name: "initMenu",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Title",
+                "Update Employee Manager",
+                new inquirer.Separator(),
+                "View All Departments",
+                "Add Department",
+                "Remove Department",
+                new inquirer.Separator(),
+                "View All Roles",
+                "Add Role",
+                "Remove Role",
+                new inquirer.Separator(),
+                "Exit",
+                new inquirer.Separator()
+            ]
+        }).then(response => {
+            switch (response.initMenu) {
+
+                case "View All Employees":
+                    return queryEmployees("all");
+
+                case "View All Employees By Department":
+                    return queryEmployees("department");
+
+                case "View All Employees By Manager":
+                    return queryEmployees("manager");
+
+                case "Exit":
+                    return connection.end();
+            }
+        });
+
+}
+
+// Show Employees
+function queryEmployees(filter) {
+
+    // All employees
+    if (filter === "all") {
+        let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, role.department_id, employee.manager_id, department.name FROM employee "
+        query += "INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) ORDER BY employee.id ASC;";
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            //console.log(res);
+            let employeeArray = [];
+            let manager = "";
+            res.forEach(employee => {
+                if (employee.manager_id != null) {
+                    manager = `${res[employee.manager_id].first_name} ${res[employee.manager_id].last_name}`
+                } else {
+                    manager = "null"
+                }
+                const newEmployee = new Employee(employee.id, employee.first_name, employee.last_name, employee.title, employee.name, employee.salary, manager);
+                employeeArray.push(newEmployee);
+            });
+            console.table(employeeArray);
+            initMenu();
+        });
+    }
+
+    // Employees by manager
+    if (filter === "manager") {
+        let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, role.department_id, employee.manager_id, department.name FROM employee "
+        query += "INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) ORDER BY employee.id ASC;";
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            //console.log(res);
+            let employeeArray = [];
+            let manager = "";
+            res.forEach(employee => {
+                if (employee.manager_id != null) {
+                    manager = `${res[employee.manager_id].first_name} ${res[employee.manager_id].last_name}`
+                } else {
+                    manager = "null"
+                }
+                const newEmployee = new Employee(employee.id, employee.first_name, employee.last_name, employee.title, employee.name, employee.salary, manager);
+                employeeArray.push(newEmployee);
+            });
+            console.table(employeeArray);
+            initMenu();
+        });
+    }
+
+    // Employees by department
+    if (filter === "department") {
+        let query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, role.department_id, employee.manager_id, department.name FROM employee "
+        query += "INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) ORDER BY employee.id ASC;";
+        connection.query(query, function (err, res) {
+            if (err) throw err;
+            //console.log(res);
+            let employeeArray = [];
+            let manager = "";
+            res.forEach(employee => {
+                if (employee.manager_id != null) {
+                    manager = `${res[employee.manager_id].first_name} ${res[employee.manager_id].last_name}`
+                } else {
+                    manager = "null"
+                }
+                const newEmployee = new Employee(employee.id, employee.first_name, employee.last_name, employee.title, employee.name, employee.salary, manager);
+                employeeArray.push(newEmployee);
+            });
+            console.table(employeeArray);
+            initMenu();
+        });
+    }
+
+}
+
+// Create Employee
+function addEmployee() {
+
+}
+
+// Remove Employee
+function removeEmployee() {
+    
+}
+
+// Update Employee
+function updateEmployee(type) {
+    if (type === "title") {
+
+    }
+
+    if (type === "manager") {
+
+    }    
+}
--- seed.sql
+++ seed.sql
@@ -1,79 +1,43 @@
-DROP DATABASE IF EXISTS employeetracker_db;
-
-CREATE DATABASE employeetracker_db;
-
 USE employeetracker_db;
 
-CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE role (
-  id INT NOT NULL,
-  title VARCHAR(30) NULL,
-  salary DECIMAL NULL,
-  department_id INT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
-
 -- Department
-INSERT INTO department (id, name)
-VALUES (1, "Sales");
+INSERT INTO department (name)
+VALUES ("Sales");
 
-INSERT INTO department (id, name)
-VALUES (2, "Engineering");
+INSERT INTO department (name)
+VALUES ("Engineering");
 
-INSERT INTO department (id, name)
-VALUES (3, "Finance");
+INSERT INTO department (name)
+VALUES ("Finance");
 
-INSERT INTO department (id, name)
-VALUES (4, "Legal");
+INSERT INTO department (name)
+VALUES ("Legal");
 
 -- Role
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (2, "Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (3, "Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (4, "Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (5, "Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (6, "Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (7, "Lawyer", 190000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4);
 
 -- Employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 3);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 2, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Ashley", "Rodriguez", 3, null);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Tupik", 4, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Malia", "Brown", 5, null);
@@ -82,7 +46,16 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Sarah", "Lourd", 6, null);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Allen", 7, 6);
+VALUES ("John", "Doe", 1, 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Christian", "Eckenrode", 3, 2);
+VALUES ("Mike", "Chan", 2, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 7, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Christian", "Eckenrode", 3, 5);
